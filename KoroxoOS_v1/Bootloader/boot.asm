af64b7c9e8aec12f3d875723cb19b10a635e4570
kb_swastik <76393434+Swastik-Sarkar@users.noreply.github.com>
2022-10-24 20:38:09 +0530

    Update boot.asm

--- KoroxoOS_v1/Bootloader/boot.asm
+++ KoroxoOS_v1/Bootloader/boot.asm
@@ -1,374 +1,342 @@
 ; ==================================================================
 ; Copyright (C) 2006 - 2010 MikeOS Developers -- see doc/LICENSE.TXT
 ;
-; This is loaded from the floppy/CD, by BOOTLOAD.BIN, as KERNEL.BIN.
-; First we have the system call vectors, which start at a static point
-; for programs to jump to. Following that is the main kernel code and
-; then additional system call code is included.
+; Based on a free boot loader by E Dehling. It scans the FAT12
+; floppy for KERNEL.BIN (the MikeOS kernel), loads it and executes it.
+; This must grow no larger than 512 bytes (one sector), with the final
+; two bytes being the boot signature (AA55h). Note that in FAT12,
+; a cluster is the same as a sector: 512 bytes.
 ; ==================================================================
 
 
 	BITS 16
 
-	%DEFINE TINKEROS_VER '4.1'	; OS version number
-	%DEFINE TINKEROS_API_VER 13	; API version for programs to check
-
-
-	; This is the location in RAM for kernel disk operations, 24K
-	; after the point where the kernel has loaded; it's 8K in size,
-	; because external programs load after it at the 32K point:
-
-	disk_buffer	equ	24576
+	jmp short bootloader_start	; Jump past disk description section
+	nop				; Pad out before disk description
 
 
 ; ------------------------------------------------------------------
-; OS CALL VECTORS -- Static locations for system call vectors
-; Note: these cannot be moved, or it'll break the calls!
+; Disk description table, to make it a valid floppy
+; Note: some of these values are hard-coded in the source!
+; Values are those used by IBM for 1.44 MB, 3.5" diskette
 
-; The comments show exact locations of instructions in this section,
-; and are used in programs/mikedev.inc so that an external program can
-; use a MikeOS system call without having to know its exact position
-; in the kernel source code...
-
-os_call_vectors:
-	jmp os_main			; 0000h -- Called from bootloader
-	jmp os_print_string		; 0003h
-	jmp os_move_cursor		; 0006h
-	jmp os_clear_screen		; 0009h
-	jmp os_print_horiz_line		; 000Ch
-	jmp os_print_newline		; 000Fh
-	jmp os_wait_for_key		; 0012h
-	jmp os_check_for_key		; 0015h
-	jmp os_int_to_string		; 0018h
-	jmp os_speaker_tone		; 001Bh
-	jmp os_speaker_off		; 001Eh
-	jmp os_load_file		; 0021h
-	jmp os_pause			; 0024h
-	jmp os_fatal_error		; 0027h
-	jmp os_draw_background		; 002Ah
-	jmp os_string_length		; 002Dh
-	jmp os_string_uppercase		; 0030h
-	jmp os_string_lowercase		; 0033h
-	jmp os_input_string		; 0036h
-	jmp os_string_copy		; 0039h
-	jmp os_dialog_box		; 003Ch
-	jmp os_string_join		; 003Fh
-	jmp os_get_file_list		; 0042h
-	jmp os_string_compare		; 0045h
-	jmp os_string_chomp		; 0048h
-	jmp os_string_strip		; 004Bh
-	jmp os_string_truncate		; 004Eh
-	jmp os_bcd_to_int		; 0051h
-	jmp os_get_time_string		; 0055h
-	jmp os_get_api_version		; 0057h
-	jmp os_file_selector		; 005Ah
-	jmp os_get_date_string		; 005Dh
-	jmp os_send_via_serial		; 0060h
-	jmp os_get_via_serial		; 0063h
-	jmp os_find_char_in_string	; 0066h
-	jmp os_get_cursor_pos		; 0069h
-	jmp os_print_space		; 006Ch
-	jmp os_dump_string		; 006Fh
-	jmp os_print_digit		; 0072h
-	jmp os_print_1hex		; 0075h
-	jmp os_print_2hex		; 0078h
-	jmp os_print_4hex		; 007Bh
-	jmp os_long_int_to_string	; 007Eh
-	jmp os_long_int_negate		; 0081h
-	jmp os_set_time_fmt		; 0084h
-	jmp os_set_date_fmt		; 0087h
-	jmp os_show_cursor		; 008Ah
-	jmp os_hide_cursor		; 008Dh
-	jmp os_dump_registers		; 0090h
-	jmp os_string_strincmp		; 0093h
-	jmp os_write_file		; 0096h
-	jmp os_file_exists		; 0099h
-	jmp os_create_file		; 009Ch
-	jmp os_remove_file		; 009Fh
-	jmp os_rename_file		; 00A2h
-	jmp os_get_file_size		; 00A5h
-	jmp os_input_dialog		; 00A8h
-	jmp os_list_dialog		; 00ABh
-	jmp os_string_reverse		; 00AEh
-	jmp os_string_to_int		; 00B1h
-	jmp os_draw_block		; 00B4h
-	jmp os_get_random		; 00B7h
-	jmp os_string_charchange	; 00BAh
-	jmp os_serial_port_enable	; 00BDh
-	jmp os_sint_to_string		; 00C0h
-	jmp os_string_parse		; 00C3h
-	jmp os_run_basic		; 00C6h
-	jmp os_port_byte_out		; 00C9h
-	jmp os_port_byte_in		; 00CCh
+OEMLabel		db "TINKERBOOT"	; Disk label
+BytesPerSector		dw 512		; Bytes per sector
+SectorsPerCluster	db 1		; Sectors per cluster
+ReservedForBoot		dw 1		; Reserved sectors for boot record
+NumberOfFats		db 2		; Number of copies of the FAT
+RootDirEntries		dw 224		; Number of entries in root dir
+					; (224 * 32 = 7168 = 14 sectors to read)
+LogicalSectors		dw 2880		; Number of logical sectors
+MediumByte		db 0F0h		; Medium descriptor byte
+SectorsPerFat		dw 9		; Sectors per FAT
+SectorsPerTrack		dw 18		; Sectors per track (36/cylinder)
+Sides			dw 2		; Number of sides/heads
+HiddenSectors		dd 0		; Number of hidden sectors
+LargeSectors		dd 0		; Number of LBA sectors
+DriveNo			dw 0		; Drive No: 0
+Signature		db 41		; Drive signature: 41 for floppy
+VolumeID		dd 00000000h	; Volume ID: any number
+VolumeLabel		db "TINKEROS     "; Volume Label: any 11 chars
+FileSystem		db "FAT12   "	; File system type: don't change!
 
 
 ; ------------------------------------------------------------------
-; START OF MAIN KERNEL CODE
+; Main bootloader code
 
-os_main:
-	cli				; Clear interrupts
-	mov ax, 0
-	mov ss, ax			; Set stack segment and pointer
-	mov sp, 0FFFFh
+bootloader_start:
+	mov ax, 07C0h			; Set up 4K of stack space above buffer
+	add ax, 544			; 8k buffer = 512 paragraphs + 32 paragraphs (loader)
+	cli				; Disable interrupts while changing stack
+	mov ss, ax
+	mov sp, 4096
 	sti				; Restore interrupts
 
-	cld				; The default direction for string operations
-					; will be 'up' - incrementing address in RAM
+	mov ax, 07C0h			; Set data segment to where we're loaded
+	mov ds, ax
 
-	mov ax, 2000h			; Set all segments to match where kernel is loaded
-	mov ds, ax			; After this, we don't need to bother with
-	mov es, ax			; segments ever again!
-	mov fs, ax
-	mov gs, ax
+	; NOTE: A few early BIOSes are reported to improperly set DL
 
-	mov ax, 1003h			; Set text output with certain attributes
-	mov bx, 0			; to be bright, and not blinking
-	int 10h
+	mov byte [bootdev], dl		; Save boot device number
 
-	call os_seed_random		; Seed random number generator
+	mov eax, 0			; Needed for some older BIOSes
 
 
-	; First up, let's see if there's a file called AUTORUN.BIN and execute
-	; it if so, before going to the program launcher menu
+; First, we need to load the root directory from the disk. Technical details:
+; Start of root = ReservedForBoot + NumberOfFats * SectorsPerFat = logical 19
+; Number of root = RootDirEntries * 32 bytes/entry / 512 bytes/sector = 14
+; Start of user data = (start of root) + (number of root) = logical 33
 
-	mov ax, autorun_bin_file_name
-	call os_file_exists
-	jc no_autorun_bin		; Skip next three lines if AUTORUN.BIN doesn't exist
+floppy_ok:				; Ready to read first block of data
+	mov ax, 19			; Root dir starts at logical sector 19
+	call l2hts
 
-	mov cx, 32768			; Otherwise load the program into RAM...
-	call os_load_file
-	jmp execute_bin_program		; ...and move on to the executing part
-
-
-	; Perhaps there's an AUTORUN.BAS however?
-
-no_autorun_bin:
-	mov ax, autorun_bas_file_name
-	call os_file_exists
-	jc option_screen		; Skip next five lines if AUTORUN.BAS doesn't exist
-
-	mov cx, 32768			; Otherwise load the program into RAM
-	call os_load_file
-	call os_clear_screen
-	mov ax, 32768
-	call os_run_basic		; Run the kernel's BASIC interpreter
-
-	jmp app_selector		; And go to the app selector menu when BASIC ends
-
-
-	; Now we display a dialog box offering the user a choice of
-	; a menu-driven program selector, or a command-line interface
-
-option_screen:
-	mov ax, os_init_msg		; Set up the welcome screen
-	mov bx, os_version_msg
-	mov cx, 10011111b		; Colour: white text on light blue
-	call os_draw_background
-
-	mov ax, dialog_string_1		; Ask if user wants app selector or command-line
-	mov bx, dialog_string_2
-	mov cx, dialog_string_3
-	mov dx, 1			; We want a two-option dialog box (OK or Cancel)
-	call os_dialog_box
-
-	cmp ax, 1			; If OK (option 0) chosen, start app selector
-	jne near app_selector
-
-	call os_clear_screen		; Otherwise clean screen and start the CLI
-	call os_command_line
-
-	jmp option_screen		; Offer menu/CLI choice after CLI has exited
-
-
-	; Data for the above code...
-
-	os_init_msg		db 'Welcome to the Tinkernut OS', 0
-	os_version_msg		db 'Version ', TINKEROS_VER, 0
-
-	dialog_string_1		db 'Thanks for trying out Tinkernut OS!', 0
-	dialog_string_2		db 'Please select an interface: OK for the', 0
-	dialog_string_3		db 'program menu, Cancel for command line.', 0
-
-
-
-app_selector:
-	mov ax, os_init_msg		; Draw main screen layout
-	mov bx, os_version_msg
-	mov cx, 10011111b		; Colour: white text on light blue
-	call os_draw_background
-
-	call os_file_selector		; Get user to select a file, and store
-					; the resulting string location in AX
-					; (other registers are undetermined)
-
-	jc option_screen		; Return to the CLI/menu choice screen if Esc pressed
-
-	mov si, ax			; Did the user try to run 'KERNEL.BIN'?
-	mov di, kern_file_name
-	call os_string_compare
-	jc no_kernel_execute		; Show an error message if so
-
-
-	; Next, we need to check that the program we're attempting to run is
-	; valid -- in other words, that it has a .BIN extension
-
-	push si				; Save filename temporarily
-
+	mov si, buffer			; Set ES:BX to point to our buffer (see end of code)
+	mov bx, ds
+	mov es, bx
 	mov bx, si
-	mov ax, si
-	call os_string_length
 
-	mov si, bx
-	add si, ax			; SI now points to end of filename...
+	mov ah, 2			; Params for int 13h: read floppy sectors
+	mov al, 14			; And read 14 of them
 
-	dec si
-	dec si
-	dec si				; ...and now to start of extension!
-
-	mov di, bin_ext
-	mov cx, 3
-	rep cmpsb			; Are final 3 chars 'BIN'?
-	jne not_bin_extension		; If not, it might be a '.BAS'
-
-	pop si				; Restore filename
+	pusha				; Prepare to enter loop
 
 
-	mov ax, si
-	mov cx, 32768			; Where to load the program file
-	call os_load_file		; Load filename pointed to by AX
+read_root_dir:
+	popa				; In case registers are altered by int 13h
+	pusha
+
+	stc				; A few BIOSes do not set properly on error
+	int 13h				; Read sectors using BIOS
+
+	jnc search_dir			; If read went OK, skip ahead
+	call reset_floppy		; Otherwise, reset floppy controller and try again
+	jnc read_root_dir		; Floppy reset OK?
+
+	jmp reboot			; If not, fatal double error
 
 
-execute_bin_program:
-	call os_clear_screen		; Clear screen before running
+search_dir:
+	popa
 
-	mov ax, 0			; Clear all registers
+	mov ax, ds			; Root dir is now in [buffer]
+	mov es, ax			; Set DI to this info
+	mov di, buffer
+
+	mov cx, word [RootDirEntries]	; Search all (224) entries
+	mov ax, 0			; Searching at offset 0
+
+
+next_root_entry:
+	xchg cx, dx			; We use CX in the inner loop...
+
+	mov si, kern_filename		; Start searching for kernel filename
+	mov cx, 11
+	rep cmpsb
+	je found_file_to_load		; Pointer DI will be at offset 11
+
+	add ax, 32			; Bump searched entries by 1 (32 bytes per entry)
+
+	mov di, buffer			; Point to next entry
+	add di, ax
+
+	xchg dx, cx			; Get the original CX back
+	loop next_root_entry
+
+	mov si, file_not_found		; If kernel is not found, bail out
+	call print_string
+	jmp reboot
+
+
+found_file_to_load:			; Fetch cluster and load FAT into RAM
+	mov ax, word [es:di+0Fh]	; Offset 11 + 15 = 26, contains 1st cluster
+	mov word [cluster], ax
+
+	mov ax, 1			; Sector 1 = first sector of first FAT
+	call l2hts
+
+	mov di, buffer			; ES:BX points to our buffer
+	mov bx, di
+
+	mov ah, 2			; int 13h params: read (FAT) sectors
+	mov al, 9			; All 9 sectors of 1st FAT
+
+	pusha				; Prepare to enter loop
+
+
+read_fat:
+	popa				; In case registers are altered by int 13h
+	pusha
+
+	stc
+	int 13h				; Read sectors using the BIOS
+
+	jnc read_fat_ok			; If read went OK, skip ahead
+	call reset_floppy		; Otherwise, reset floppy controller and try again
+	jnc read_fat			; Floppy reset OK?
+
+	mov si, disk_error		; If not, print error message and reboot
+	call print_string
+	jmp reboot			; Fatal double error
+
+
+read_fat_ok:
+	popa
+
+	mov ax, 2000h			; Segment where we'll load the kernel
+	mov es, ax
 	mov bx, 0
-	mov cx, 0
+
+	mov ah, 2			; int 13h floppy read params
+	mov al, 1
+
+	push ax				; Save in case we (or int calls) lose it
+
+
+; Now we must load the FAT from the disk. Here's how we find out where it starts:
+; FAT cluster 0 = media descriptor = 0F0h
+; FAT cluster 1 = filler cluster = 0FFh
+; Cluster start = ((cluster number) - 2) * SectorsPerCluster + (start of user)
+;               = (cluster number) + 31
+
+load_file_sector:
+	mov ax, word [cluster]		; Convert sector to logical
+	add ax, 31
+
+	call l2hts			; Make appropriate params for int 13h
+
+	mov ax, 2000h			; Set buffer past what we've already read
+	mov es, ax
+	mov bx, word [pointer]
+
+	pop ax				; Save in case we (or int calls) lose it
+	push ax
+
+	stc
+	int 13h
+
+	jnc calculate_next_cluster	; If there's no error...
+
+	call reset_floppy		; Otherwise, reset floppy and retry
+	jmp load_file_sector
+
+
+	; In the FAT, cluster values are stored in 12 bits, so we have to
+	; do a bit of maths to work out whether we're dealing with a byte
+	; and 4 bits of the next byte -- or the last 4 bits of one byte
+	; and then the subsequent byte!
+
+calculate_next_cluster:
+	mov ax, [cluster]
 	mov dx, 0
-	mov si, 0
-	mov di, 0
+	mov bx, 3
+	mul bx
+	mov bx, 2
+	div bx				; DX = [cluster] mod 2
+	mov si, buffer
+	add si, ax			; AX = word in FAT for the 12 bit entry
+	mov ax, word [ds:si]
 
-	call 32768			; Call the external program code,
-					; loaded at second 32K of segment
-					; (program must end with 'ret')
+	or dx, dx			; If DX = 0 [cluster] is even; if DX = 1 then it's odd
 
-	call os_clear_screen		; When finished, clear screen
-	jmp app_selector		; and go back to the program list
+	jz even				; If [cluster] is even, drop last 4 bits of word
+					; with next cluster; if odd, drop first 4 bits
 
-
-no_kernel_execute:			; Warn about trying to executing kernel!
-	mov ax, kerndlg_string_1
-	mov bx, kerndlg_string_2
-	mov cx, kerndlg_string_3
-	mov dx, 0			; One button for dialog box
-	call os_dialog_box
-
-	jmp app_selector		; Start over again...
+odd:
+	shr ax, 4			; Shift out first 4 bits (they belong to another entry)
+	jmp short next_cluster_cont
 
 
-not_bin_extension:
-	pop si				; We pushed during the .BIN extension check
-
-	push si				; Save it again in case of error...
-
-	mov bx, si
-	mov ax, si
-	call os_string_length
-
-	mov si, bx
-	add si, ax			; SI now points to end of filename...
-
-	dec si
-	dec si
-	dec si				; ...and now to start of extension!
-
-	mov di, bas_ext
-	mov cx, 3
-	rep cmpsb			; Are final 3 chars 'BAS'?
-	jne not_bas_extension		; If not, error out
+even:
+	and ax, 0FFFh			; Mask out final 4 bits
 
 
-	pop si
+next_cluster_cont:
+	mov word [cluster], ax		; Store cluster
 
-	mov ax, si
-	mov cx, 32768			; Where to load the program file
-	call os_load_file		; Load filename pointed to by AX
+	cmp ax, 0FF8h			; FF8h = end of file marker in FAT12
+	jae end
 
-	call os_clear_screen		; Clear screen before running
-
-	mov ax, 32768
-	call os_run_basic		; And run our BASIC interpreter on the code!
-
-	mov si, basic_finished_msg
-	call os_print_string
-	call os_wait_for_key
-
-	call os_clear_screen
-	jmp app_selector		; and go back to the program list
+	add word [pointer], 512		; Increase buffer pointer 1 sector length
+	jmp load_file_sector
 
 
-not_bas_extension:
-	pop si
+end:					; We've got the file to load!
+	pop ax				; Clean up the stack (AX was pushed earlier)
+	mov dl, byte [bootdev]		; Provide kernel with boot device info
 
-	mov ax, ext_string_1
-	mov bx, ext_string_2
-	mov cx, 0
-	mov dx, 0			; One button for dialog box
-	call os_dialog_box
-
-	jmp app_selector		; Start over again...
-
-
-	; And now data for the above code...
-
-	kern_file_name		db 'KERNEL.BIN', 0
-
-	autorun_bin_file_name	db 'AUTORUN.BIN', 0
-	autorun_bas_file_name	db 'AUTORUN.BAS', 0
-
-	bin_ext			db 'BIN'
-	bas_ext			db 'BAS'
-
-	kerndlg_string_1	db 'Cannot load and execute TinkerOS kernel!', 0
-	kerndlg_string_2	db 'KERNEL.BIN is the core of TinkerOS, and', 0
-	kerndlg_string_3	db 'is not a normal program.', 0
-
-	ext_string_1		db 'Invalid filename extension! You can', 0
-	ext_string_2		db 'only execute .BIN or .BAS programs.', 0
-
-	basic_finished_msg	db '>>> BASIC program finished -- press a key', 0
+	jmp 2000h:0000h			; Jump to entry point of loaded kernel!
 
 
 ; ------------------------------------------------------------------
-; SYSTEM VARIABLES -- Settings for programs and system calls
+; BOOTLOADER SUBROUTINES
+
+reboot:
+	mov ax, 0
+	int 16h				; Wait for keystroke
+	mov ax, 0
+	int 19h				; Reboot the system
 
 
-	; Time and date formatting
+print_string:				; Output string in SI to screen
+	pusha
 
-	fmt_12_24	db 0		; Non-zero = 24-hr format
+	mov ah, 0Eh			; int 10h teletype function
 
-	fmt_date	db 0, '/'	; 0, 1, 2 = M/D/Y, D/M/Y or Y/M/D
-					; Bit 7 = use name for months
-					; If bit 7 = 0, second byte = separator character
+.repeat:
+	lodsb				; Get char from string
+	cmp al, 0
+	je .done			; If char is zero, end of string
+	int 10h				; Otherwise, print it
+	jmp short .repeat
+
+.done:
+	popa
+	ret
+
+
+reset_floppy:		; IN: [bootdev] = boot device; OUT: carry set on error
+	push ax
+	push dx
+	mov ax, 0
+	mov dl, byte [bootdev]
+	stc
+	int 13h
+	pop dx
+	pop ax
+	ret
+
+
+l2hts:			; Calculate head, track and sector settings for int 13h
+			; IN: logical sector in AX, OUT: correct registers for int 13h
+	push bx
+	push ax
+
+	mov bx, ax			; Save logical sector
+
+	mov dx, 0			; First the sector
+	div word [SectorsPerTrack]
+	add dl, 01h			; Physical sectors start at 1
+	mov cl, dl			; Sectors belong in CL for int 13h
+	mov ax, bx
+
+	mov dx, 0			; Now calculate the head
+	div word [SectorsPerTrack]
+	mov dx, 0
+	div word [Sides]
+	mov dh, dl			; Head/side
+	mov ch, al			; Track
+
+	pop ax
+	pop bx
+
+	mov dl, byte [bootdev]		; Set correct device
+
+	ret
 
 
 ; ------------------------------------------------------------------
-; FEATURES -- Code to pull into the kernel
+; STRINGS AND VARIABLES
+
+	kern_filename	db "KERNEL  BIN"	; TinkerOS kernel filename
+
+	disk_error	db "Floppy error! Press any key...", 0
+	file_not_found	db "KERNEL.BIN not found!", 0
+
+	bootdev		db 0 	; Boot device number
+	cluster		dw 0 	; Cluster of the file we want to load
+	pointer		dw 0 	; Pointer into Buffer, for loading kernel
 
 
-	%INCLUDE "features/cli.asm"
-	%INCLUDE "features/basic.asm"
- 	%INCLUDE "features/disk.asm"
-	%INCLUDE "features/keyboard.asm"
-	%INCLUDE "features/math.asm"
-	%INCLUDE "features/misc.asm"
-	%INCLUDE "features/ports.asm"
-	%INCLUDE "features/screen.asm"
-	%INCLUDE "features/sound.asm"
-	%INCLUDE "features/string.asm"
+; ------------------------------------------------------------------
+; END OF BOOT SECTOR AND BUFFER START
+
+	times 510-($-$$) db 0	; Pad remainder of boot sector with zeros
+	dw 0AA55h		; Boot signature (DO NOT CHANGE!)
 
 
-; ==================================================================
-; END OF KERNEL
+buffer:				; Disk buffer begins (8k after this, stack starts)
+
+
 ; ==================================================================
 
